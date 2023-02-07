991c20666840788ef64ac532f598a9daa3bf821a
Dionisio <39993447+dbnvides@users.noreply.github.com>
2023-02-06 21:02:41 -0300

    fix: security database

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,11 @@
 import { Client, QueryConfig, QueryResult } from "pg";
 
 export const client: Client = new Client({
-  user: "Estudo Kenzie",
-  password: "1234",
-  host: "localhost",
-  database: "movies",
-  port: 5432,
+  user: ,
+  password: ,
+  host: ,
+  database: ,
+  port: ,
 });
 
 export const startDatabase = async (): Promise<void> => {
